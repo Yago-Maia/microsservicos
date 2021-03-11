4e8261e4f1329cc3f81d7987b312cc5386abf3e9
Yago Maia <yagogmaia@gmail.com>
2021-03-10 22:18:12 -0300

    Criação serviço Pagamento

---
+++ pagamento/src/test/java/com/yagomaia/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagomaia.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
