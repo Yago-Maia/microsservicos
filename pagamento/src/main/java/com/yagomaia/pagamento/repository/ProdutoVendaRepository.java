a06c9f5f5d354bc73c2017643d597455bd6d2a9b
Yago Maia <yagogmaia@gmail.com>
2021-03-10 22:47:12 -0300

    Criação dos repositorys

---
+++ pagamento/src/main/java/com/yagomaia/pagamento/repository/ProdutoVendaRepository.java
@@ -0,0 +1,9 @@
+package com.yagomaia.pagamento.repository;
+
+import com.yagomaia.pagamento.entity.ProdutoVenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoVendaRepository extends JpaRepository<ProdutoVenda, Long> {
+}
