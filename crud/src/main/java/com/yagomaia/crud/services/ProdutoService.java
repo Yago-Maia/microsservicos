09c497ffc213d9caa3bde74576d490f957777cd3
Yago Maia <yagogmaia@gmail.com>
2021-04-07 22:08:28 -0300

    CRUD - Inclusão do RabbitMQ

--- crud/src/main/java/com/yagomaia/crud/services/ProdutoService.java
+++ crud/src/main/java/com/yagomaia/crud/services/ProdutoService.java
@@ -3,6 +3,7 @@ package com.yagomaia.crud.services;
 import com.yagomaia.crud.data.vo.ProdutoVO;
 import com.yagomaia.crud.entity.Produto;
 import com.yagomaia.crud.exception.ResourceNotFoundException;
+import com.yagomaia.crud.message.ProdutoSendMessage;
 import com.yagomaia.crud.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,14 +16,18 @@ import java.util.Optional;
 public class ProdutoService {
 
     private final ProdutoRepository produtoRepository;
+    private final ProdutoSendMessage produtoSendMessage;
 
     @Autowired
-    public ProdutoService(ProdutoRepository produtoRepository) {
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoSendMessage produtoSendMessage) {
         this.produtoRepository = produtoRepository;
+        this.produtoSendMessage = produtoSendMessage;
     }
 
     public ProdutoVO create(ProdutoVO produtoVO) {
-        return ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+        ProdutoVO produtoVORetorno = ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+        produtoSendMessage.sendMessage(produtoVO);
+        return produtoVORetorno;
     }
 
     public Page<ProdutoVO> findAll(Pageable pageable) {
@@ -43,7 +48,7 @@ public class ProdutoService {
     public ProdutoVO update(ProdutoVO produtoVO) {
         final Optional<Produto> optionalProduto = produtoRepository.findById(produtoVO.getId());
 
-        if(!optionalProduto.isPresent()) {
+        if(optionalProduto.isEmpty()) {
             throw new ResourceNotFoundException("Nenhum produto encontrado para esse ID.");
         }
 
